d5ac3f6fff2beff1c2ef5f060e9bf227139b1d9a
krishna <krishna04.b@gmailo.com>
2025-09-16 14:07:56 +0545

    java

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 10;  // first number
+        int b = 20;  // second number
+        System.out.println("The sum is: " + (a+b));
+    }
+}
+
